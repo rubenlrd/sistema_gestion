12384eb75c717eefc7855d5283a1812943cae0d9
Ruben Leiva <rubenlrd@gmail.com>
2025-06-13 00:12:36 -0300

    se agrego la capa de theme ttkbootstrap

--- domain/wind_login.py
+++ domain/wind_login.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import ttk, messagebox
+import ttkbootstrap as tb 
 
 
 class LoginWindow(tk.Tk):
@@ -10,21 +11,22 @@ class LoginWindow(tk.Tk):
         self.geometry("250x150")
         self.resizable(False, False)
         self.attributes("-toolwindow", True)
+        
 
         # Ingresa usuario
-        tk.Label(self, text="Usuario:").grid(row=0, column=0, padx=10, pady=10, sticky="e")
-        self.entry_user = tk.Entry(self)
+        tb.Label(self, text="Usuario:").grid(row=0, column=0, padx=10, pady=10, sticky="e")
+        self.entry_user = tb.Entry(self)
         self.entry_user.grid(row=0, column=1, padx=10, pady=10)
 
         # Ingresa contraseña
-        tk.Label(self, text="Contraseña:").grid(row=1, column=0, padx=10, pady=10, sticky="e")
-        self.entry_pass = tk.Entry(self, show="*")
+        tb.Label(self, text="Contraseña:").grid(row=1, column=0, padx=10, pady=10, sticky="e")
+        self.entry_pass = tb.Entry(self, show="*")
         self.entry_pass.grid(row=1, column=1, padx=10, pady=10)
 
-        frame_botones = tk.Frame(self)
+        frame_botones = tb.Frame(self)
         frame_botones.grid(row=2, column=1, columnspan=2, pady=10)
-        btn_login = tk.Button(frame_botones, text="Entrar")  # command=self.login
+        btn_login = tb.Button(frame_botones, text="Entrar")  # command=self.login
         btn_login.pack(side="left", padx=5)
-        btn_cerrar = tk.Button(frame_botones, text="Cerrar", command=self.destroy)  # Sin paréntesis
+        btn_cerrar = tb.Button(frame_botones, text="Cerrar", command=self.destroy)  # Sin paréntesis
         btn_cerrar.pack(side="left", padx=5)
 
